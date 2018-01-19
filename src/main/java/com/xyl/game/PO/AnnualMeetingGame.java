ae38b7a96c4a175f8024984be886780fb5307fd2
naah <nayan3480232@163.com>
2018-01-19 20:01:36 +0800

    ...

---
+++ src/main/java/com/xyl/game/PO/AnnualMeetingGame.java
@@ -0,0 +1,24 @@
+package com.xyl.game.PO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnnualMeetingGame {
+    private String id;
+    private String topic;
+    private String answer_one;
+    private String answer_twe;
+    private String answer_three;
+    private String answer_fore;
+    private String right_answer;
+    private String create_time;
+}
+
+
+
+
+
+
+
