ceb49643efc4ccab5a4375680b6904372472d1f6
naah <nayan3480232@163.com>
2018-01-19 19:00:30 +0800

    ...

---
+++ src/main/java/com/xyl/game/GameApplication.java
@@ -0,0 +1,12 @@
+package com.xyl.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xyl/game/GameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xyl.game;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
