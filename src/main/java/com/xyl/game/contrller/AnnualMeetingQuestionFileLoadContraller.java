78559b1d2417ceed437eaa2bb6c009fbbfb998dc
赵振智 <zhaozhenzhi@xinyunlian.com>
2018-01-20 18:08:01 +0800

    11

--- src/main/java/com/xyl/game/contrller/AnnualMeetingQuestionFileLoadContraller.java
+++ src/main/java/com/xyl/game/contrller/AnnualMeetingQuestionFileLoadContraller.java
@@ -25,7 +25,6 @@ public class AnnualMeetingQuestionFileLoadContraller {
 
 	@Autowired 
 	private AnnualMeetingQuestionExctFileSerivce exctFileSerivce;
-
 	
 	@RequestMapping("/loadExctFile")
 	@ResponseBody
